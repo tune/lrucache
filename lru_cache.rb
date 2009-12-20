282637369fdece1d3bb8883d6986cdeabc7c82d4
Yuichi TSUNEMATSU <yuichi@tsunematsu.cc>
2009-12-20 13:31:57 +0900

    TDD Boot Campでの最終版

--- lru_cache.rb
+++ lru_cache.rb
@@ -1,41 +1,85 @@
+# LRUCache
+# 一定の数に達したら使われてない順に要素が削除されていくMapのような入れ物
 class LRUCache
-	attr_reader :max_size
+	attr_accessor :max_size # 最大記憶サイズ
+	attr_accessor :expire   # データの有効期限(ミリ秒)
 
-	def initialize( max_size )
-		raise LRUCacheException if max_size == nil
+	def initialize( max_size = 10 )
+    raise LRUCacheException unless max_size.is_a?(Fixnum)
 		raise LRUCacheException if max_size <= 0
 
 		@max_size = max_size
 		@queue = []
+    @expire = 360000
 	end
+    
+  def max_size=(max_size)
+    @max_size = max_size
+    size_wipeout
+  end
 
 	# keyとvalueを関連付けて記録
 	def put(key, value)
-		@queue << {key => value}
-		
-		if @queue.size > max_size then
-			@queue.shift
-		end
+    @queue.delete_if {|v| v.has_key?(key)}
+		@queue << KVS.new(key, value)
+    size_wipeout
 	end
 
 	# keyに対応する値を返す
 	def get(key)
-		@queue.each do |element|
-			if element.has_key?(key)
-				@queue.delete(element)
-				@queue << element
-				return element[key]
+    time_wipeout
+		@queue.each do |v|
+			if v.has_key?(key)
+				@queue.delete(v)
+        v.touch
+				@queue << v
+				return v.value
 			end
 		end
-	    return nil
+	  nil
 	end
 	
 	# 最も参照されていないキーを返す
-	def last_recently_used
-		@queue[0].keys[0]
+	def oldest_key
+    raise LRUCacheException if @queue.first.nil?
+    @queue.first.key
 	end
+
+  private
+
+  def size_wipeout
+    @queue = @queue.last(max_size)
+  end
+
+  def time_wipeout(now = Time.now)
+    @queue.delete_if do |i|
+      (now - i.time) * 1000 > @expire
+    end
+  end
+
+  class KVS
+
+    attr_reader :key, :value, :time
+
+    def initialize(key, value)
+      @key = key
+      @value = value
+      touch
+    end
+
+    def has_key?(key)
+      @key == key
+    end
+
+    def touch
+      @time = Time.now
+    end
+
+  end
+
 end
 
+# 例外クラス
 class LRUCacheException < RuntimeError
 end
 
--- lru_cache_spec.rb
+++ lru_cache_spec.rb
@@ -4,7 +4,7 @@ require 'spec'
 require 'lru_cache'
 
 describe LRUCache do
-	describe "初期化関連" do
+  describe "初期化関連" do
 		before (:each) do
 			@lru = nil
 		end
@@ -12,6 +12,11 @@ describe LRUCache do
 		it 'クラスが初期化出来ること' do
 			@lru = LRUCache.new(2)
 		end
+
+    it '初期値が省略されると、デフォルト値が使われる' do
+			@lru = LRUCache.new
+      @lru.max_size.should == 10
+    end
 	
 		it '初期化子に与えたLRUCacheのサイズが正しいこと' do
 			@lru = LRUCache.new(2)
@@ -27,13 +32,44 @@ describe LRUCache do
 				LRUCache.new(-1)
 			}.should raise_error(LRUCacheException)
 		end 
+    
+    it '整数以外が渡された場合、LRUCacheExceptionを返す' do
+			lambda{
+				LRUCache.new("1")
+			}.should raise_error(LRUCacheException)
+			lambda{
+				LRUCache.new(2.2)
+			}.should raise_error(LRUCacheException)
+    end
 
 		it '上限の指定がない場合、LRUCacheExceptionを返す' do
 			lambda{
 				LRUCache.new(nil)
 			}.should raise_error(LRUCacheException)
 		end
-	end
+  end
+
+  describe "記憶サイズの変更" do
+		before (:each) do
+			@lru = LRUCache.new(2)
+		end
+
+    it "記憶サイズを増やす" do 
+			@lru.put("a", "dataA")
+			@lru.put("b", "dataB")
+      @lru.max_size = 3
+			@lru.put("c", "dataC")
+			@lru.get("a").should == "dataA"
+    end
+
+    it "記憶サイズを減らす" do 
+			@lru.put("a", "dataA")
+			@lru.put("b", "dataB")
+      @lru.max_size = 1
+			@lru.get("a").should be_nil
+    end
+
+  end
 
 	describe "put, getの操作関連" do
 		before (:each) do
@@ -47,39 +83,88 @@ describe LRUCache do
 			@lru.get("b").should == "dataB"
 		end
 	
-	    it 'keyとして登録されていないものはnilが返ること' do
-	      @lru.put("a", "dataA");
-	      @lru.get("b").should be_nil
-	    end
+	  it 'keyとして登録されていないものはnilが返ること' do
+	    @lru.put("a", "dataA")
+	    @lru.get("b").should be_nil
+	  end
 	
-	    it 'keyがnilの場合も設定した値が返ること' do
-	      @lru.put(nil, "nilobject")
-	      @lru.get(nil).should == "nilobject"
-	    end
+    it 'keyがnilの場合も設定した値が返ること' do
+      @lru.put(nil, "nilobject")
+      @lru.get(nil).should == "nilobject"
+    end
 	    
-	    it '値を３つ以上登録すると最後に登録したものが残っていること' do
-			@lru.put("a", "dataA")
-			@lru.put("b", "dataB")
-			@lru.put("c", "dataC")
-	        @lru.get("c").should == "dataC"
-		end
-    
-	    it "getで値を参照すると最も参照されてないキーが更新されること" do
+    it '値を３つ以上登録すると最後に登録したものが残っていること' do
+	    @lru.put("a", "dataA")
+	    @lru.put("b", "dataB")
+	    @lru.put("c", "dataC")
+      @lru.get("c").should == "dataC"
+    end
+   
+    it "getで値を参照すると最も参照されてないキーが更新されること" do
 			@lru.put("a", "dataA")
 			@lru.put("b", "dataB")
 			@lru.get("a")
-			@lru.last_recently_used.should == "b"
+			@lru.oldest_key.should == "b"
 			@lru.get("b")
-			@lru.last_recently_used.should == "a"
+			@lru.oldest_key.should == "a"
+		end
+
+    it "追加済みのキーを更新できる" do
+			@lru.put("a", "dataA")
+			@lru.put("b", "dataB")
+			@lru.put("a", "dataa")
+			@lru.oldest_key.should == "b"
+			@lru.get("a").should == "dataa"
 		end
     
-	    it '値を4つ登録すると最初に登録したkeyを参照するとnilが返ること' do
+	  it '値を4つ登録すると最初に登録したkeyを参照するとnilが返ること' do
 			@lru.put("a", "dataA")
 			@lru.put("b", "dataB")
 			@lru.put("c", "dataC")
 			@lru.put("d", "dataD")
 			@lru.get("a").should be_nil
 		end
+
+    it "未登録時にもっとも参照されていないキーを取り出すと例外が投げられる" do
+			lambda{
+				@lru.oldest_key 
+			}.should raise_error(LRUCacheException)
     end
+
+  end
+
+  describe "一定時間過ぎたデータの削除" do
+		before (:each) do
+			@lru = LRUCache.new(3)
+		end
+
+    it "記憶時間の初期値が設定されていること" do
+      @lru.expire.should == 360000
+    end
+
+    it "記憶時間の設定メソッドのテスト" do
+      @lru.expire = 300
+      @lru.expire.should == 300
+    end
+
+    it "一定時間過ぎてキャッシュが削除されていることのテスト" do
+      @lru.expire = 100
+			@lru.put("a", "dataA")
+      sleep(0.2)
+			@lru.get("a").should be_nil
+    end
+
+    it "一定時間内に参照があれば、追加時刻を更新" do
+      @lru.expire = 100
+			@lru.put("a", "dataA")
+      sleep(0.05)
+			@lru.get("a").should == "dataA"
+      sleep(0.06)
+			@lru.get("a").should == "dataA"
+      sleep(0.2)
+			@lru.get("a").should be_nil
+    end
+
+  end
 end
 
